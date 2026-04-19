caae40904ca9a93758990cbf7d2b0419590168b4
nufafa <124320554+nufafa@users.noreply.github.com>
2026-04-19 09:44:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ CATEGORIES = [
         "name": "互联网资讯",
         "description": "整合互联网资讯媒体，偏重于前端科技、电子硬件、手机应用。",
         "sources": [
-            {"name": "36Kr", "url": "https://rss.aishort.top/?type=36kr"},
-            {"name": "虎嗅网", "url": "https://rss.aishort.top/?type=huxiu"},
-            {"name": "艾瑞网", "url": "https://rss.aishort.top/?type=iresearch"},
-            {"name": "爱范儿", "url": "https://rss.aishort.top/?type=AppSolution"},
-            {"name": "效率火箭", "url": "https://rss.aishort.top/?type=xlrocket"},
+            {"name": "《联合早报》-国际-即时", "url": "https://plink.anyfeeder.com/zaobao/realtime/world"},
+            {"name": "极客公园", "url": "http://www.geekpark.net/rss"},
+            {"name": "ZAKER 精读新闻", "url": "https://rsshub.app/zaker/focusread"},
+            {"name": "36氪", "url": "https://www.36kr.com/feed"},
+            {"name": "今日话题 - 雪球", "url": "https://plink.anyfeeder.com/xueqiu/hot"},
         ]
     },
     {
         "name": "奇思妙想",
         "description": "每天吸收些稀奇古怪的知识，期待引发后续的灵机一动。",
         "sources": [
-            {"name": "果壳网", "url": "https://rss.aishort.top/?type=guokr"},
-            {"name": "少数派", "url": "https://rss.aishort.top/?type=sspai"},
-            {"name": "知乎想法热榜", "url": "https://rss.aishort.top/?type=zhihu"},
+            {"name": "知乎日报", "url": "https://plink.anyfeeder.com/zhihu/daily"},
+            {"name": "好奇心日报", "url": "http://www.qdaily.com/feed.xml"},
+            {"name": "知乎每日精选", "url": "https://www.zhihu.com/rss"},
         ]
     }
 ]
